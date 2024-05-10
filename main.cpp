6426d867d65568b763bdd41c6eb456149f384e55
Ewaldo <iguachente@gmail.com>
2024-05-10 00:35:20 -0400

    Update 10/05/24 00:34
    Adición de Prueba del menú con interacción de teclas del jugador e implementación de menú y demás procedimientos en el main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 
 //#include <stdio.h>
 #include <iostream>
-#include <string>
+#include <cstdlib>
+#include <string> 
+#include <windows.h>
 using namespace std;
 
 //Declaración de Estructuras
@@ -18,6 +20,7 @@ struct Recursos{
 struct Jugadores{
     int pts;
     int equipo;
+    int posicion
     string nombre_equipo;
     string nombre_jugador;
     Recursos *inventario;
@@ -53,14 +56,102 @@ Jugadores *CrearJugador(string nombre){
 
 //CONTROL DE ARCHIVOS
 
-//INTERACCIONES USUARIO-PROGRAMA
+//TABLERO
+
+Casillas* crearCasilla(int valor) {
+  Casillas *nuevo = new Casillas;
+  nuevo->id_casillas = valor;
+  nuevo->prox = NULL;
+  return nuevo;
+}//funcion para crear los nodos del tablero(lista)
+
+bool TableroVacio(Casillas *inicio){
+    return inicio==NULL;
+}
+
+void mostrartablero(Casillas *Tablero){
+   Casillas *mover=Tablero;
+      while (mover != NULL)
+        {
+           cout <<" | "<< mover->id_casillas<<" | " <<"->";
+           mover = mover->prox;
+         }
+         cout<<"F I N"<<endl;
+   }  
+
+void llenado_tablero(Casillas *&Tablero,int iddelacasilla){ //llenado de archivo de forma manual sin uso de archivos
+    Casillas *CasillaNueva=crearCasilla(iddelacasilla);
+    if(TableroVacio(Tablero)){
+        Tablero=CasillaNueva;
+    }else{
+        Casillas *auxiliar_mover=Tablero;
+        while(auxiliar_mover->prox!=NULL){
+            auxiliar_mover=auxiliar_mover->prox;
+        }
+        auxiliar_mover->prox=CasillaNueva;
+    }
+}
 
 //CICLOS MENU/JUEGO/RONDA/TURNO
 
+void imprime_opciones_menu(){
+    cout<<"A:  Iniciar Partida"<<endl;
+    cout<<"C:  Tutorial/Instrucciones"<<endl;
+    cout<<"B:  Salir del Juego"<<endl;
+}
+
+void imprime_instrucciones(){
+
+}
+
+void Turno(/*parámetros*/){ //acaba cuando el jugador elija su acción
+
+}
+
+void Ronda(/*parámetros*/){ //acaba al haber terminado el Turno de cada jugador
+
+}
+
+void Partida(/*parámetros*/){ //acaba cuando se cumple la condicion de fin de partida, ya sea victoria o salida forzada
+
+}
+
+void MainMenu(/*parámetros*/){// acaba cuando se decida cerrar el juego(programa) por completo
+    bool fin_partida=false;
+    int opcion;
+    cout<<"Que opcion desea escoger?";
+    imprime_opciones_menu();
+    cin>>opcion;
+    switch (opcion){
+    case 1: //INICIA PARTIDA
+        Partida(); //In
+    break;
+    
+    case 2: //INICIA INSTRUCCIONES/REGLAS/TURORIAL
+        imprime_instrucciones();
+    break;
+    
+    default:
+        cout<<"Opcion Inválida..."<<endl;
+    break;
+    }
+}
+void Mover_Jugador(Jugadores jugador){
+    Jugadores *JugadorN= CrearJugador()
+
+}
+
 //PROGRAMA PRINCIPAL
 main(){
-    string palabra;
-    cout<<"digalo ";
-    cin>>palabra;
+    int idtablero=1,i=0;
+    Casillas *Tablero=NULL;
+    while(i<25){
+        llenado_tablero(Tablero,idtablero);
+        idtablero++;
+        i++;
+    };
+    mostrartablero(Tablero);
+
+   /// MainMenu(/*parámetros*/);  //Llamamos al ciclo general de la partida
     
 }
